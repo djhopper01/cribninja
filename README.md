2136f801f6032281a96081dab6d34cb805599f48
Derek Hopper <hopper.derek@gmail.com>
2012-09-22 09:34:42 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Campus Cribs?
+
+### college apartment hunting sucks. we make it better.
+
+###### v1 built during 48 hours [#48hfw](https://twitter.com/#48hfw) brought to you by [Founders](http://atfounders.com)
+
+[Todd Garland](http://toddgarland.com):
+> Openly share and talk to people about your idea. Use their lack of interest or doubt to fuel your motivation to make it happen.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cribs::Application.config.secret_token = '0d8c5d802a436d9fbc63620f8a462ccee548e507143a0fdc5781082c136160b26c2573a440b1323efa2c944d6b77a0c153780bcb36f43b229643c0cf1a399651'
