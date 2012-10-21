7a26e173a609a328e8ee1064dea4e755676cb7f4
Derek Hopper <hopper.derek@gmail.com>
2012-10-20 20:50:33 -0400

    Ability to login with Facebook.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Cribs::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { 
+    :omniauth_callbacks => "users/omniauth_callbacks" 
+  }
 
   resources :welcome
 
